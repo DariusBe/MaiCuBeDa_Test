a0c9b6690aa3df684cd280fac8749188acde444a
gelatinfossils <d-bchr@protonmail.com>
2024-06-08 15:26:19 +0200

    added collision model

--- Physarum/simple_animation.py
+++ Physarum/simple_animation.py
@@ -4,12 +4,14 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 # global variables:
-grid_size = 250
+grid_size = 350
 trail_decay = 0.9
+trail_factor = 1.0
 step_width = 1.0
 rotation_angle = 22.5
 sensor_angles = 45.0
 sensor_distances = 9.0
+physarum_count = 800
 
 
 # define numpy 2D Vector
@@ -27,46 +29,69 @@ class Physarum:
         # randomly around +/- rotation_angle
         self.heading += np.random.uniform(-rotation_angle, rotation_angle)
     
-    def move(self,step_width):
+    def move(self, step_width):
         self.position.x += step_width * np.cos(np.radians(self.heading))
         self.position.y += step_width * np.sin(np.radians(self.heading))
-
-
-
+        # if position is outside the grid, set it to the edge of the grid
+        self.position.x = min(max(0, self.position.x), grid_size - 1)
+        self.position.y = min(max(0, self.position.y), grid_size - 1)
+        # invert heading if position is at the edge of the grid
+        if self.position.x in [0, grid_size - 1]:
+            self.heading = 180 - self.heading
+        if self.position.y in [0, grid_size - 1]:
+            self.heading = -self.heading
+    
+    # is move possible?
+    def can_move(self, position):
+        # check if at future position, there is aleady a Physarum
+        future_pos = pos_to_grid(Vec2D(position.x + step_width * np.cos(np.radians(self.heading)), position.y + step_width * np.sin(np.radians(self.heading))))
+        if grid[future_pos] > 0:
+            return False
+        return True
+        
 # map position to grid
-def pos_to_grid(pos):
+def pos_to_grid(pos, grid_size=grid_size):
+    # make sure the position is within the grid
+    pos.x = min(max(0, pos.x), grid_size - 1)
+    pos.y = min(max(0, pos.y), grid_size - 1)
     return int(pos.x), int(pos.y)
 
 grid = np.zeros((grid_size, grid_size))
-grid[grid_size//2, grid_size//2] = 0.000001
+grid[grid_size//2, grid_size//2] = 0.000000001 
 
-# initialize the grid with some low values
-trail = np.random.normal(-0.25, 0.25, size=(grid_size, grid_size))
-# cut off the negative values
-trail = np.clip(trail, 0, 1)
+# randomly create few spots with aggregated trail
+trail = np.random.poisson(0.01, (grid_size, grid_size))
+# randomize values > 0 in trail to values between 0.1 and 1.0
+trail = np.where(trail > 0, np.random.uniform(0.1, 1.0, trail.shape), trail)
 
 particles = []
-for i in range(1):
-    particles.append(Physarum(Vec2D(np.random.normal(scale=grid_size), np.random.normal(scale=grid_size)), np.random.randint(0, 360)))
+for i in range(physarum_count):
+    # spawn physarum from center of the grid
+    rand_x = grid_size // 2 + np.random.uniform(-grid_size // 50, grid_size // 50)
+    rand_y = grid_size // 2 + np.random.uniform(-grid_size // 50, grid_size // 50)
+
+    rand_heading = np.random.choice(np.arange(0, 360, rotation_angle))
+    
+    particles.append(Physarum(Vec2D(rand_x, rand_y), rand_heading))
 
 def update(last_grid, rotation_angle=rotation_angle, step_width=step_width):
     grid = last_grid.copy()
     # Loop over all cells in the grid and place one Physarum
     for physarum in particles:
-        physarum.position.x = physarum.position.x % grid_size
-        physarum.position.y = physarum.position.y % grid_size
         # Get the position of the Physarum in the grid
-        pos_x, pos_y = pos_to_grid(physarum.position)
+        pos_x, pos_y = pos_to_grid(physarum.position, grid_size)
         grid[pos_x, pos_y] = 1
+
         
         physarum.rotate(rotation_angle)
-        physarum.move(step_width)
+        if physarum.can_move(physarum.position):
+            physarum.move(step_width)
     return grid
 
 
-
 fig = plt.figure()
-im = plt.imshow(grid, cmap='terrain', animated=True)
+# stack trail on top of the grid
+im = plt.imshow(grid, cmap='cividis', animated=True)
 speed_slider = Slider(plt.axes([0.1, 0.01, 0.8, 0.03]), 'Physarum Step Width', 0.001, 10.0, valinit=step_width)
 rotation_slider = Slider(plt.axes([0.1, 0.05, 0.8, 0.03]), 'Physarum Rotation Angle', 0, 90, valinit=rotation_angle)
 def animate(*args):
@@ -77,45 +102,10 @@ def animate(*args):
     rotation_angle = rotation_slider.val
     grid = update(grid, rotation_angle, step_width)
     im.set_array(grid)
-    return im,
-ani = animation.FuncAnimation(fig, animate, interval=5, blit=True)
-plt.show()
-
-
-'''
-# Initialize grid
-#grid = np.random.choice([0, 1], size=(50, 50))
-grid = np.random.choice([0,0,0,0,1], size=(150, 150))
-
-
-# Define the rules
-def update(grid):
-    new_grid = grid.copy()
-    # Loop over all cells in the grid
-    for i in range(1, grid.shape[0] - 1):
-        for j in range(1, grid.shape[1] - 1):
-            # If the cell is empty, leave it empty
-            if grid[i, j] == 0:
-                new_grid[i, j] = 0
-            # If the cell is full, check if it has 5 or more neighbors
-            else:
-                neighbors = grid[i - 1:i + 2, j - 1:j + 2]
-                # If it has 3 or more neighbors, fill the cell
-                if np.sum(neighbors) >= 3:
-                    new_grid[i, j] = 1
-                # If it has 8 neighbors, empty the neighbors
-                else:
-                    new_grid[i, j] = 0
-    return new_grid
-
-# Create the animation
-fig = plt.figure()
-im = plt.imshow(grid, cmap='gray', animated=True)
-def animate(*args):
-    global grid
-    grid = update(grid)
+    # multiply pixels by trail
     im.set_array(grid)
     return im,
-ani = animation.FuncAnimation(fig, animate, interval=500, blit=True)
+ani = animation.FuncAnimation(fig, animate, interval=1, blit=True)
+# show colorbar
+plt.colorbar(im)
 plt.show()
-'''
