e86a5fee3b89dfe13a2a770505f7581058a197bd
gelatinfossils <d-bchr@protonmail.com>
2024-06-09 17:35:20 +0200

    cleaned update function

--- Physarum/PhysarumSimulation.py
+++ Physarum/PhysarumSimulation.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-from Utils import xyzFileToImg, Vec2D, pos_to_grid
+from Utils import xyzFileToImg, Vec2D, pos_to_grid, blurMap
 from Physarum import Physarum
 
 class PhysarumSimulation:
@@ -21,58 +21,65 @@ class PhysarumSimulation:
         self.grid = np.zeros((grid_size, grid_size))
         self.trail = np.zeros((grid_size, grid_size))
         self.sensor_map = np.zeros((grid_size, grid_size))
-        self.penalty_map = self.createPenaltyMap() #xyzFileToImg(filepath, self.grid_size)
+        self.penalty_map = xyzFileToImg(filepath, self.grid_size, 3)
+        self.oat_map = self.createOatMap()
         self.population = []
 
     # if no file is given, create a blank grid
-    def createPenaltyMap(self):
-        # randomly create few spots with aggregated trail
-        penalty_map = np.random.poisson(0.01, (grid_size, grid_size))
-        # randomize values > 0 in trail to values between 0.1 and 1.0
-        penalty_map = np.where(penalty_map > 0, np.random.uniform(0.1, 1.0, penalty_map.shape), penalty_map)
-        return penalty_map
+    def createOatMap(self):
+        oatMap = np.zeros((self.grid_size, self.grid_size))
+        # create a few circles with random radius and position
+        for i in range(3):
+            radius = np.random.randint(4, 8)
+            x = np.random.randint(self.grid_size//2, self.grid_size)
+            y = np.random.randint(0, self.grid_size)
+            cv2.circle(oatMap, (x, y), radius, 5, -1)
+        return oatMap*6
 
     def populate(self):
         population = []
         for i in range(self.physarus_count):
             rand_x = self.grid_size // 2 + np.random.uniform(-self.grid_size // 50, self.grid_size // 50)
             rand_y = self.grid_size // 2 + np.random.uniform(-self.grid_size // 50, self.grid_size // 50)
-            rand_heading = np.random.choice(np.arange(0, 360, self.rotation_angle))
+            rand_heading = np.random.choice([-self.rotation_angle, self.rotation_angle])
             # def __init__(self, position, heading, sensor_distance, sensor_width, sensor_angle):
             population.append(Physarum(Vec2D(rand_x, rand_y), rand_heading, self.sensor_distance, self.sensor_width, self.sensor_angle))
         self.population = population
 
-
-
-    def update(self, old_grid):
-        grid = old_grid.copy()
+    def attemptMove(self, grid):
         # Loop over all cells in the grid and place one Physarum
         for physarum in self.population:
             # Get the position of the Physarum in the grid
             pos_x, pos_y = pos_to_grid(physarum.position, self.grid_size)
-            #grid[pos_x-2:pos_x+2, pos_y-2:pos_y+2] = 0.25
             grid[pos_x, pos_y] = 1
 
             # attempt to move
             if physarum.can_move(self.trail, physarum.position, self.step_width):
                 physarum.move(self.step_width, self.grid_size)
-                # get new position
                 physarum.rotate(self.rotation_angle)
                 pos_x, pos_y = pos_to_grid(physarum.position, self.grid_size)
                 self.trail[pos_x, pos_y] = 1
             else:
-                rand_heading = np.random.choice(np.arange(0, 360, self.rotation_angle))
+                rand_heading = np.random.choice([-self.rotation_angle, self.rotation_angle])
+                #rand_heading = np.random.choice(np.arange(0, 360, self.rotation_angle))
                 physarum.heading = rand_heading
+        return grid
 
-
-            # sense
+    def senseAndConquer(self):
+        for physarum in self.population:
             self.sensor_map = np.zeros((self.grid_size, self.grid_size))
             self.sensor_map = physarum.showSensorPositions(self.sensor_map)
-
-            sensor_values = physarum.sense(self.trail + self.penalty_map)
-
-            # apply attraction rules
+            sensor_values = physarum.sense(self.trail + self.oat_map)
             physarum.attraction_rules(self.step_width, self.grid_size, sensor_values)
+
+    def update(self, old_grid):
+        grid = old_grid.copy()
+        # shuffle population
+        np.random.shuffle(self.population)
+        # Loop over all cells in the grid and place one Physarum
+        grid = self.attemptMove(grid)
+        np.random.shuffle(self.population)
+        self.senseAndConquer()
         return grid
     
     def runSimulation(self, interval):
@@ -82,34 +89,50 @@ class PhysarumSimulation:
         fig, ax = plt.subplots()
         ax.set_xlim(0, self.grid_size)
         ax.set_ylim(0, self.grid_size)
-        im = ax.imshow(self.grid, cmap='cividis', origin='lower')
+        # stack the images
+        #im1 = ax.imshow(self.oat_map, cmap='gray', origin='lower')
+        #m2 = ax.imshow(self.trail, cmap='gray', origin='lower')
+        im = ax.imshow(self.trail, cmap='cividis', origin='lower')
 
+        # create sliders
         decay_slider = Slider(plt.axes([0.5, 0.01, 0.25, 0.02]), 'Penalty Decay Rate', 0.5, 1.0, valinit=self.trail_decay)
+        rotation_angle_slider = Slider(plt.axes([0.5, 0.04, 0.25, 0.02]), 'Rotation Angle', 0, 180, valinit=self.rotation_angle)
+        step_width_slider = Slider(plt.axes([0.5, 0.07, 0.25, 0.02]), 'Step Width', 0.5, 5, valinit=self.step_width)
+        sensor_distance_slider = Slider(plt.axes([0.5, 0.10, 0.25, 0.02]), 'Sensor Distance', 5, 20, valinit=self.sensor_distance)
+        sensor_angle_slider = Slider(plt.axes([0.5, 0.13, 0.25, 0.02]), 'Sensor Angle', 22.5, 90, valinit=self.sensor_angle)
+        # make space for the sliders
+        plt.subplots_adjust(left=0.1, bottom=0.2)
 
 
         def animate(*args):
             global grid
-            trail_decay = decay_slider.val
-            self.trail = self.update(self.trail) * trail_decay
+            self.trail_decay = decay_slider.val
+            self.rotation_angle = rotation_angle_slider.val
+            self.step_width = step_width_slider.val
+            self.sensor_distance = sensor_distance_slider.val
+            self.sensor_angle = sensor_angle_slider.val
+            #self.oat_map -= (self.trail * 0.01)
 
-
-            im.set_array(self.trail + self.penalty_map)
+            # diffuse the trail using cv2.dilate
+            self.trail += self.oat_map
+            self.trail = self.update(self.trail) * self.trail_decay
+            im.set_array(self.trail)
             return im,
 
-        ani = animation.FuncAnimation(fig, animate, interval=interval)
+        ani = animation.FuncAnimation(fig, animate, cache_frame_data=False, interval=interval, blit=True)
         plt.colorbar(im)
         plt.show()
 
         
 
 
-filepath = "Physarum/geo_data/dgm_33250-5888.xyz"
-grid_size = 250
+filepath = "Physarum/geo_data/dgm_33250-5889.xyz"
+grid_size = 100
 trail_decay = 0.9
-population = 1000
+population = 250
 rotation_angle = 22.5
 step_width = 1
-sensor_distance = 9
+sensor_distance = 10
 sensor_angle = 45
 sensor_width = 1
 
--- Physarum/Utils.py
+++ Physarum/Utils.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -22,13 +23,13 @@ z = z.reshape(nr_cols, nr_rows)
 x = x[:nr_cols]
 y = y[::nr_rows]
 
-'''
-# plot the topography
-plt.figure()
-plt.imshow(z, cmap='terrain', origin='lower')
-plt.colorbar()
-plt.show()
-'''
+
+# # plot the topography
+# plt.figure()
+# plt.imshow(z, cmap='gray', origin='lower')
+# plt.colorbar()
+# plt.show()
+
 
 # calculate the gradient of the topography
 dx, dy = np.gradient(z)
@@ -37,18 +38,18 @@ mag = np.sqrt(dx**2 + dy**2)
 # normalize the gradient
 mag = (mag - mag.min()) / (mag.max() - mag.min())
 
-'''
-# plot the gradient
-plt.figure()
-plt.imshow(mag, cmap='gray', origin='lower')
-plt.colorbar()
-plt.show()
-'''
+
+# # plot the gradient
+# plt.figure()
+# plt.imshow(mag, cmap='terrain', origin='lower')
+# plt.colorbar()
+# plt.show()
+
 
 # save normalized gradient as a heightmap (0-255)
 plt.imsave(filepath.split('.')[0] + '_gradient.png', mag, cmap='gray')
 
-def xyzFileToImg(filepath, grid_size):
+def xyzFileToImg(filepath, grid_size, factor=1.0):
     topo = np.loadtxt(filepath, delimiter=',')
     # get the x and y coordinates
     x = topo[:,0] # means all rows, only the first column
@@ -65,11 +66,13 @@ def xyzFileToImg(filepath, grid_size):
     # normalize the gradient
     norm = (mag - mag.min()) / (mag.max() - mag.min())
     # create trail map from normalized gradient
-    trail = np.zeros((grid_size, grid_size))
+    penalty = np.zeros((grid_size, grid_size))
     for i in range(grid_size):
         for j in range(grid_size):
-            trail[i, j] = norm[int(i * nr_cols / grid_size), int(j * nr_rows / grid_size)]
-    return trail
+            penalty[i, j] = norm[int(i * nr_cols / grid_size), int(j * nr_rows / grid_size)]
+    # corrode the penalty map
+    penalty = cv2.GaussianBlur(penalty, (5, 5), 0)
+    return penalty*factor
 
 # define numpy 2D Vector
 class Vec2D:
@@ -83,3 +86,11 @@ def pos_to_grid(pos, grid_size=100):
     pos.x = min(max(0, pos.x), grid_size - 1)
     pos.y = min(max(0, pos.y), grid_size - 1)
     return int(pos.x), int(pos.y)
+
+# only lightly blur the penalty map using convolution
+def blurMap(penalty_map, grid_size, scale_factor=1.0):
+    kernel = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]]) / 9 * scale_factor
+    print(kernel.shape, kernel)
+    blurred_map = cv2.filter2D(penalty_map, -1, kernel)
+    return blurred_map
+
